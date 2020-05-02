d6b350de3dac31302d8847732c68e5ce2e648d05
YaminiVignesh <yaminiparanthaman@gmail.com>
2020-05-02 17:43:44 +0530

    first code of project5

---
+++ src/main/java/org/cts/MavenProject5/App.java
@@ -0,0 +1,13 @@
+package org.cts.MavenProject5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/cts/project5/Sample5.java
@@ -0,0 +1,8 @@
+package org.cts.project5;
+
+public class Sample5 {
+public static void main(String[] args) {
+	System.out.println("Git");
+	
+}
+}
